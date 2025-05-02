6b04fda494947335ae0fdbfd78f898173dbc34bd
AartiSGulhane <aartigulhane18@gmail.com>
2025-05-02 17:19:28 +0530

    add m2

--- GitDemo2/src/com/demo/Test.java
+++ GitDemo2/src/com/demo/Test.java
@@ -4,6 +4,9 @@ public class Test {
 	public void m1() {
 		System.out.println("hi...its local repository..");
 	}
+	public void m2() {
+		System.out.println("m2 from local repository");
+	}
 
 	public static void main(String[] args) {
 	
